801f257b9b438e896cbd51a32dc2a1214b5be6eb
Talati <stalati@deloitte.com>
2021-04-25 01:24:47 -0400

    first commit

---
+++ Demo/src/Latte.java
@@ -0,0 +1,12 @@
+
+public class Latte {
+
+	String name;   
+    Latte(){   
+         return;   
+    }   
+    Latte(String name){   
+         this.name=name;   
+         return;   
+    }   
+}
---
+++ Demo/src/Student.java
@@ -0,0 +1,36 @@
+
+public class Student {
+
+	private String id;
+	private String name;
+	private int score;
+	
+	
+	
+	public Student(String id, String name, int score) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.score = score;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
+}
